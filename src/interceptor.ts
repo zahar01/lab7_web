bb7a95bffd10e6e2c7a619a4613ab765c2ac0930
zahar01 <42346793+zahar01@users.noreply.github.com>
2022-03-25 20:12:54 +0300

    class rename

--- src/interceptor.ts
+++ src/interceptor.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable()
-export class LoggingInterceptor implements NestInterceptor {
+export class Interceptor implements NestInterceptor {
   intercept(
     context: ExecutionContext,
     next: CallHandler,
