6cf94633a703aad371382aa502d15b6d2caa2c09
zahar01 <42346793+zahar01@users.noreply.github.com>
2022-04-15 12:30:19 +0300

    class rename

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { Interceptor } from './interceptor';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'ec2-52-30-67-143.eu-west-1.compute.amazonaws.com',
+      port: 5432,
+      username: 'rjkeodderxszol',
+      password: '048274445a91d812f5ea1b8aba312f6f10f3f5d6c4f4d563c6c2d013f6746ede',
+      database: 'd74qjtmv65e4jk',
+      entities: [],
+      synchronize: true,
+      ssl: { rejectUnauthorized: false }
+    }),
+    ItemsModule,
+  ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_INTERCEPTOR, useClass: Interceptor }],
 })
