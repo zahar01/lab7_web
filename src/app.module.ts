20e2594df1405cdf5ad8c460f52ff559baea5018
zahar01 <42346793+zahar01@users.noreply.github.com>
2022-04-15 13:30:50 +0300

    Revert "class rename"
    This reverts commit bb7a95bffd10e6e2c7a619a4613ab765c2ac0930.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import { Interceptor } from './interceptor';
+import { LoggingInterceptor } from './logging.interceptor';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, { provide: APP_INTERCEPTOR, useClass: Interceptor }],
+  providers: [
+    AppService,
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+  ],
 })
 export class AppModule {}
