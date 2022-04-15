c5f23c36dc50e53b7a13fbac927a3a7c31bc53c3
zahar01 <42346793+zahar01@users.noreply.github.com>
2022-04-15 13:47:24 +0300

    !

--- src/items/dto/add-item.dto.ts
+++
@@ -1,8 +0,0 @@
-export class AddItemDto{
-  readonly name:string;
-  readonly label:string;
-  readonly description:string;
-  readonly price:number;
-  readonly category:string;
-  readonly gender:string;
-}
